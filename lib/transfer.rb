65829595bba2a6a20724c9a0754608dbd05fd6e5
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-23 05:54:12 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,9 @@ class Transfer
       @receiver.balance += @amount 
       @status = "complete"
     end
-    "Transaction rejected. Please check your account balance."
+    if @status != "complete" 
+      "Transaction rejected. Please check your account balance."
+    end
   end 
   
 end
