f7341db255c73bf8b40a38b27be290acd83195a4
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-23 05:51:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
   
   def execute_transaction
-    unless @status = "complete" 
+    unless @status == "complete" 
       @sender.balance -= @amount 
       @receiver.balance += @amount 
       @status = "complete"
