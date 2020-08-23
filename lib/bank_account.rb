932192097d77785e6faf8c356e2c41f03a828ac4
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-23 05:23:36 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,6 +1,6 @@
 class BankAccount
-  attr_accessor :balance,:status
-  attr_reader :name
+  attr_accessor :name, :balance,:status
+  # attr_reader :name
   
   def initialize(name,balance=100,status=open)
     @name = name
